ed28a5c19b0c4c12295ea697f4d1d5845bef1a8b
Laurel768 <laurel.whitten@hotmail.com>
2020-11-02 18:21:00 -0500

    added department data

---
+++ employeesdata.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+SELECT * FROM departments;
