bb47b322ef38863bc060e4bae89b3f195ce3f8ca
Laurel768 <laurel.whitten@hotmail.com>
2020-11-03 13:44:11 -0500

    added titles table and data

--- QuickDBD-Laurel's files.sql
+++ QuickDBD-Laurel's files.sql
@@ -1,3 +1,8 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/MxGTN6
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
 CREATE TABLE "departments" (
     "dept_no" VARCHAR(4)   NOT NULL,
     "dept_name" VARCHAR(255)   NOT NULL,
@@ -14,4 +19,3 @@ CREATE TABLE "titles" (
      )
 );
 
-SELECT * FROM titles
---
+++ QuickDBD-export (1).sql
@@ -0,0 +1,10 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/MxGTN6
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_name" VARCHAR(255)   NOT NULL
+);
+
